c69cf5a88f8e379f12315927f847821cf8a11efd
avalanchas <da.heuschkel@gmail.com>
2018-01-12 20:10:28 +0100

    more basics

--- src/main/java/com/elliecoding/nasalookout/screens/FragmentOverview.java
+++ src/main/java/com/elliecoding/nasalookout/screens/FragmentOverview.java
@@ -1,5 +1,15 @@
 package com.elliecoding.nasalookout.screens;
 
-public class FragmentOverview {
-    
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.View;
+
+public class FragmentOverview extends Fragment {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
 }
