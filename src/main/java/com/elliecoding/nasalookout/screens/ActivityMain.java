8dd9244a9396ab4e8e8992da68c5238303c966e5
avalanchas <da.heuschkel@gmail.com>
2018-01-12 20:02:52 +0100

    more basics

--- src/main/java/com/elliecoding/nasalookout/screens/ActivityMain.java
+++ src/main/java/com/elliecoding/nasalookout/screens/ActivityMain.java
@@ -7,6 +7,13 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.elliecoding.nasalookout.R;
 
 public class ActivityMain extends AppCompatActivity {
@@ -24,6 +31,35 @@ public class ActivityMain extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setHomeButtonEnabled(true);
         }
+
+        downloadPics();
+    }
+
+    private void downloadPics() {
+        final TextView mTextView = findViewById(R.id.text);
+
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.nasa.gov/planetary/apod?api_key=ht3oMoWKS6pKVaHJGFQ7IjrzkFnleJ0wTP4Hr1TQ";
+
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 
     private void initialiseDrawer() {
---
+++ src/main/java/com/elliecoding/nasalookout/screens/FragmentOverview.java
@@ -0,0 +1,5 @@
+package com.elliecoding.nasalookout.screens;
+
+public class FragmentOverview {
+    
+}
